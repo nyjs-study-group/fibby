a78be3b8691c7d9b048265f62ec95bb544b62d25
Andy <realandyfan@gmail.com>
2020-12-07 20:21:49 -0500

    fixed package.json, added more efficient fibonacci functions, added performance test

--- src/fibby.js
+++ src/fibby.js
@@ -7,3 +7,41 @@ export const fibonacci = ( count ) => {
   
     return fibonacci(count - 1) + fibonacci(count - 2);
 }
+
+
+export const fibonacciWhile = (count) => {
+    let a = 1;
+    let b = 0; 
+    let temp;
+  
+    while (count >= 0){
+      temp = a;
+      a = a + b;
+      b = temp;
+      count--;
+    }
+  
+    return b;
+  }
+
+
+export const fibonacciSeries = ( count ) => { 
+    let i = 0; 
+    const series = []; 
+    for (; i !== count; i++ ){ 
+    //caching the iterator by removing redundancy of visiting "0" 
+    series[i] = fibonacci(i) 
+    } 
+    return series 
+}
+
+
+export const fibonacciSeriesWhile = ( count ) => { 
+    let i = 0; 
+    const series = []; 
+    for (; i !== count; i++ ){ 
+    //caching the iterator by removing redundancy of visiting "0" 
+    series[i] = fibonacciWhile(i) 
+    } 
+    return series 
+}
--- src/fibby.script.js
+++ src/fibby.script.js
@@ -1,21 +1,34 @@
-import { fibonacci } from './fibby';
+import { 
+    fibonacci, 
+    fibonacciWhile, 
+    fibonacciSeries, 
+    fibonacciSeriesWhile
+} from './fibby';
 
 // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
-const fibValue1 = fibonacci( 1 ); // 1
-const fibValue2 = fibonacci( 2 ); // 2
-const fibValue3 = fibonacci( 3 ); // 2
-const fibValue4 = fibonacci( 4 ); // 3
-const fibValue5 = fibonacci( 5 ); // 5
-const fibValue6 = fibonacci( 6 ); // 8
-const fibValue7 = fibonacci( 7 ); // 13
-const fibValue100 = fibonacci( 80 );
 
-console.log( fibValue1 );
-console.log( fibValue2 );
-console.log( fibValue3 );
-console.log( fibValue4 );
-console.log( fibValue5 );
-console.log( fibValue6 );
-console.log( fibValue7 );
-console.log(' > > > > > ')
-console.log( fibValue100 );
+const start = new Date();
+
+// console.log(fibonacciSeries(60));
+console.log(fibonacciSeriesWhile(1000));
+
+const end = new Date();
+console.log(end - start);
+// const fibValue1 = fibonacci( 1 ); // 1
+// const fibValue2 = fibonacci( 2 ); // 2
+// const fibValue3 = fibonacci( 3 ); // 2
+// const fibValue4 = fibonacci( 4 ); // 3
+// const fibValue5 = fibonacci( 5 ); // 5
+// const fibValue6 = fibonacci( 6 ); // 8
+// const fibValue7 = fibonacci( 7 ); // 13
+// // const fibValue100 = fibonacci( 80 );
+
+// console.log( fibValue1 );
+// console.log( fibValue2 );
+// console.log( fibValue3 );
+// console.log( fibValue4 );
+// console.log( fibValue5 );
+// console.log( fibValue6 );
+// console.log( fibValue7 );
+// console.log(' > > > > > ')
+// // console.log( fibValue100 );

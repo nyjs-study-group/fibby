4c68ff5d9414edd24580b73067e3552ad3dc9b59
norm wiggins <normwiggins@macbook-nondev-48.myfiosgateway.com>
2020-12-09 17:22:49 -0500

    added new function using memoization

--- src/fibby.script.js
+++ src/fibby.script.js
@@ -1,14 +1,25 @@
-import { fibonacci } from './fibby';
+import { 
+    fibonacci,
+    fibonacciWhile,
+    fibonacciSeriesWhile,
+    memoFibonacci
+} from './fibby';
 
 // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
+const start = new Date();
+//console.log(fibonacciSeries(7))
+console.log( memoFibonacci(20))
+
+const end = new Date ();
+console.log(end-start);
 const fibValue1 = fibonacci( 1 ); // 1
-const fibValue2 = fibonacci( 2 ); // 2
+/*const fibValue2 = fibonacci( 2 ); // 2
 const fibValue3 = fibonacci( 3 ); // 2
 const fibValue4 = fibonacci( 4 ); // 3
 const fibValue5 = fibonacci( 5 ); // 5
 const fibValue6 = fibonacci( 6 ); // 8
 const fibValue7 = fibonacci( 7 ); // 13
-const fibValue100 = fibonacci( 80 );
+//const fibValue100 = fibonacci( 80 );
 
 console.log( fibValue1 );
 console.log( fibValue2 );
@@ -18,4 +29,5 @@ console.log( fibValue5 );
 console.log( fibValue6 );
 console.log( fibValue7 );
 console.log(' > > > > > ')
-console.log( fibValue100 );
+//console.log( fibValue100 );
+*/

5b2476c829155a294e28b03fe5291bacedf6e400
Andy <realandyfan@gmail.com>
2020-12-14 19:09:49 -0500

    Added whitelies.js, modified package.json

---
+++ src/whitelies.js
@@ -0,0 +1,35 @@
+switch(Math.floor(Math.random() * 10)){
+    case 0:
+        whitelie = "I don't really care about politics.";
+        break;
+    case 1:
+        whitelie = "Can't come in today, I'm sick.";
+        break;
+    case 2:
+        whitelie = "Size doesn't matter.";
+        break;
+    case 3:
+        whitelie = "Everything's fine.";
+        break;
+    case 4:
+        whitelie = "Sorry my phone died.";
+        break;
+    case 5:
+        whitelie = "I'll pay you back.";
+        break;
+    case 6:
+        whitelie = "I have a gluten allergy.";
+        break;
+    case 7:
+        whitelie = "The traffic was terrible.";
+        break;
+    case 8:
+        whitelie = "Of course I remember you!";
+        break;
+    case 9:
+        whitelie = "Yup, makes perfect sense!";
+        break; 
+    default: "How did we get here?";
+}
+
+console.log("Your white lie is: " + "'" + whitelie + "'");
